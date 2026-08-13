516633bd9873b770993c1be9fde5aba07e2568db
Dijash <utsavbhattarai29@gmail.com>
2026-08-13 08:55:46 +0545

    Settings Updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn chiyasan:wsgi
--- chiyasan/settings.py
+++ chiyasan/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chiyasan.apps.ChiyasanConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
